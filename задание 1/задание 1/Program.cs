a9ff618c583338525c69e0a7515c35df84b6db52
kam1len58 <kamil.228.garipov@gmail.com>
2025-02-01 11:22:38 +0300

    task completed

--- задание 1/задание 1/Program.cs
+++ задание 1/задание 1/Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 
-
-string s = Console.ReadLine();
-string n;
-string m;
-
-if (s.Length%2==0)
+class pracrice
 {
-   n=s.Substring(0, s.Length/2);
-   m=s.Substring(s.Length/2);
-   n = new string(n.Reverse().ToArray());
-   m = new string(m.Reverse().ToArray());
-   string sum = n + m;
-   Console.WriteLine(sum);
-}
-else
-{
-    n = new string(s.Reverse().ToArray());
-    string sum = n + s;
-    Console.WriteLine(sum);
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        string n;
+        string m;
+        if (s.Length % 2 == 0)
+        {
+            n = s.Substring(0, s.Length / 2);
+            m = s.Substring(s.Length / 2);
+            n = new string(n.Reverse().ToArray());
+            m = new string(m.Reverse().ToArray());
+            string sum = n + m;
+            Console.WriteLine(sum);
+        }
+        else
+        {
+            n = new string(s.Reverse().ToArray());
+            string sum = n + s;
+            Console.WriteLine(sum);
+        }
+    }
 }
 
