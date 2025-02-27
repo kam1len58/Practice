74b55de867d501253a63f7d530252f7c4d38bb60
kam1len58 <kamil.228.garipov@gmail.com>
2025-02-27 22:20:33 +0300

    task completed

---
+++ task31/task3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string s = Console.ReadLine();
+
+            bool f = false;
+            foreach (char c in s)
+            {
+                if (!alphabet.Contains(c))
+                {
+                    f = true;
+                    break;
+                }
+            }
+
+            if (f)
+            {
+                Console.WriteLine("\nНедопустимые символы: ");
+                foreach (char c in s)
+                {
+                    if (!alphabet.Contains(c))
+                    {
+                        Console.Write(c + " ");
+                    }
+                }
+                Console.WriteLine();
+                return;
+            }
+
+
+            string g = "";
+            foreach (char c in s)
+            {
+                if (alphabet.Contains(c))
+                {
+                    g += c;
+                }
+            }
+            Console.WriteLine("\nОбработанная строка:");
+            string n, m,sum;
+            if (g.Length % 2 == 0)
+            {
+                n = g.Substring(0, g.Length / 2);
+                m = g.Substring(g.Length / 2);
+                n = new string(n.Reverse().ToArray());
+                m = new string(m.Reverse().ToArray());
+                sum = n + m;
+                Console.WriteLine(sum);
+
+
+
+
+            }
+            else
+            {
+                n = new string(g.Reverse().ToArray());
+                sum = n + g;
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine("\nКол-во символов,которые встречаются в строке:");
+            int k = 0;
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                k = 0;
+                for (int i = 0; sum.Length > i; i++)
+                {
+                    if (sum[i] == c)
+                    {
+                        k++;
+                    }
+
+                }
+                if (k > 0)
+                {
+                    Console.WriteLine(c + " встречается " + k + " раз");
+                }
+
+
+            }
+        }
+
+    }
+
+}
