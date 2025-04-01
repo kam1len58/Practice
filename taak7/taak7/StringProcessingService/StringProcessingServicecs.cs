283d5022480c3475f4d3e9645345c909cd89cc87
kam1len58 <kamil.228.garipov@gmail.com>
2025-04-01 11:16:40 +0300

    Completed task7

---
+++ taak7/taak7/StringProcessingService/StringProcessingServicecs.cs
@@ -0,0 +1,28 @@
+﻿namespace taak7.StringProcessingService;
+public class StringProcessingService
+{
+    public async Task<string> RunMain(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return "HTTP ошибка 400 Bad Request";
+        }
+
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        bool hasInvalidChars = input.Any(c => !alphabet.Contains(c));
+
+        if (hasInvalidChars)
+        {
+            return "HTTP ошибка 400 Bad Request";
+        }
+
+        
+        string processedString = input.ToLower();  
+
+        return await Task.FromResult(processedString);
+    }
+}
+
+
+
+
