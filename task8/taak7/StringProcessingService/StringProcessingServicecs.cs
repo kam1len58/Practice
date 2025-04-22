4de710f76581d15bde6b6a38d2881902fba6f9d3
Kamil <kamil.228.garipov@gmal.com>
2025-04-22 10:11:09 +0300

    Added blackList

---
+++ task8/taak7/StringProcessingService/StringProcessingServicecs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+
+
+namespace taak7.StringProcessingService
+{
+    public class StringProcessingService
+    {
+        private readonly Task8 _config;
+
+
+        public StringProcessingService(IOptions<Task8> options)
+        {
+            _config = options.Value;
+        }
+
+
+        public async Task<string> RunMain(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return "HTTP ошибка 400 Bad Request. Введённая строка пуста.";
+            }
+
+
+            if (_config.BlackList != null && _config.BlackList.Any(word => input.Contains(word, StringComparison.OrdinalIgnoreCase)))
+            {
+                return "HTTP ошибка 400 Bad Request. Введённое слово находится в чёрном списке.";
+            }
+
+            string processedString = input.ToLower();
+
+
+            return await Task.FromResult(processedString);
+        }
+    }
+}
